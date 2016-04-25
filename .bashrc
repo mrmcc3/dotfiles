ffb880617f5e2ad658a28246be419aaaaf86cc13
mrmcc3 <mrmcc3@gmail.com>
2016-04-25 17:14:17 +1000

    update

--- .bashrc
+++ .bashrc
@@ -5,3 +5,8 @@ for EXT in $EXTS; do
   source $EXT
 done
 
+complete -C aws_completer aws
+
+export NVM_DIR="$HOME/.nvm"
+. "$(brew --prefix nvm)/nvm.sh"
+
--- bash/defaults.bash
+++ bash/defaults.bash
@@ -1,10 +1,7 @@
-HISTCONTROL=ignoredups:ignorespace
-shopt -s histappend
-HISTSIZE=1000
-HISTFILESIZE=2000
-shopt -s checkwinsize
 
 export PATH=$HOME/bin:$PATH
 
-export EDITOR='vim'
+export EDITOR='subl'
+
+
 
